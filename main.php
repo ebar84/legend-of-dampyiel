e670e8a1bc26361a08bf414439c44cce1b3ab3b2
ebar84 <ebar1984@gmail.com>
2023-09-07 13:48:05 -0500

    beginning of text game

--- main.php
+++ main.php
@@ -1,21 +1,31 @@
 <?php
 
 function game_start(){
+    $validChoice = true;
 
-    echo "Please make a choice\n
+    do {
+        echo "Please make a choice\n
             1.) Start a new game\n
             2.) Load Game\n
             3.) Quit\n";
 
-    $choice = readline(">> ");
+        $choice = readline(">> ");
 
-    if ($choice !== "1") {
-        echo "sorry brah, you made a bad choice.\n";
-        game_start();
-    } else {
-        echo "Game will now begin!";
-    }
-
+        switch ($choice) {
+            case "1":
+                echo "Your game is beginning\n";
+                break;
+            case "2":
+                echo "Your game is now loading\n";
+                break;
+            case "3":
+                echo "The game will now exit\n";
+                exit();
+            default:
+                $validChoice = false;
+                echo "Bad choice, brah. Please choose 1, 2, or 3\n";
+        }
+    } while (!$validChoice);
 }
 
 game_start();
