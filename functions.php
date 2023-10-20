5242c3b476353a0b54813290788e217ee5996dd5
ebar84 <ebar1984@gmail.com>
2023-10-20 12:37:42 -0500

    updates for beginning of game

--- functions.php
+++ functions.php
@@ -1,80 +1,58 @@
 <?php
 
-function game_start(){
-    $validChoice = true;
-
-    do {
-        echo "Please make a choice\n
-            1.) Start a new game\n
-            2.) Load Game\n
-            3.) Quit\n";
-
-        $choice = readline(">> ");
-
-        switch ($choice) {
-            case "1":
-                echo "Your game is beginning\n";
-                echo new_game();
-                break;
-            case "2":
-                echo "Your game is now loading\n";
-                break;
-            case "3":
-                echo "The game will now exit\n";
-                exit();
-            default:
-                $validChoice = false;
-                echo "Bad choice, brah. Please choose 1, 2, or 3\n";
-        }
-    } while (!$validChoice);
-}
-
 function new_game() {
-    $playerInfo = array();
+    do {
+        $playerInfo = array();
 
-    echo "Welcome to your new adventure!\n";
-    $playerInfo['name'] = readline("Enter your name: ");
+        echo "Welcome to your new adventure!\n";
+        $playerInfo['name'] = readline("Enter your name: ");
 
-    $validClasses = ['Warrior', 'Mage', 'Archer', 'Thief'];
-    $validRaces = ['Human', 'Elf', 'Dwarf', 'Orc'];
+        $validClasses = ['Warrior', 'Mage', 'Archer', 'Thief'];
+        $validRaces = ['Human', 'Elf', 'Dwarf', 'Orc'];
+
+        $classChoice = 0;
+        $raceChoice = 0;
+
+        do {
+            echo "Choose your class:\n";
+            for ($i = 0; $i < count($validClasses); $i++) {
+                echo ($i + 1) . ". {$validClasses[$i]}\n";
+            }
+            $classChoice = (int)readline("Choose your class (enter the number): ");
+
+            if ($classChoice < 1 || $classChoice > count($validClasses)) {
+                echo "Invalid choice. Please select a valid class number.\n";
+            }
+        } while ($classChoice < 1 || $classChoice > count($validClasses));
+
+        do {
+            echo "Choose your race:\n";
+            for ($i = 0; $i < count($validRaces); $i++) {
+                echo ($i + 1) . ". {$validRaces[$i]}\n";
+            }
+            $raceChoice = (int)readline("Choose your race (enter the number): ");
+
+            if ($raceChoice < 1 || $raceChoice > count($validRaces)) {
+                echo "Invalid choice. Please select a valid race number.\n";
+            }
+        } while ($raceChoice < 1 || $raceChoice > count($validRaces));
 
 
-    echo "Choose your class:\n";
-    for ($i = 0; $i < count($validClasses); $i++) {
-        echo ($i + 1) . ". {$validClasses[$i]}\n";
-    }
-    $classChoice = (int)readline("Choose your class (enter the number): ");
+        echo "Your awesome new hero has been created!\n";
+        echo "Name: " . $playerInfo['name'] . "\n";
+        echo "Class: " . $validClasses[$classChoice - 1] . "\n";
+        echo "Race: " . $validRaces[$raceChoice - 1] . "\n";
 
-    if ($classChoice < 1 || $classChoice > count($validClasses)) {
-        echo "Invalid choice. Please select a valid class number.\n";
-        return;
-    }
+        $playerInfo['class'] = $validClasses[$classChoice - 1];
+        $playerInfo['race'] = $validRaces[$raceChoice - 1];
 
-
-    echo "Choose your race:\n";
-    for ($i = 0; $i < count($validRaces); $i++) {
-        echo ($i + 1) . ". {$validRaces[$i]}\n";
-    }
-    $raceChoice = (int)readline("Choose your race (enter the number): ");
-
-    if ($raceChoice < 1 || $raceChoice > count($validRaces)) {
-        echo "Invalid choice. Please select a valid race number.\n";
-        return;
-    }
-
-    echo "Your awesome new hero has been created!\n";
-    echo "Name: " . $playerInfo['name'] . "\n";
-    echo "Class: " . $validClasses[$classChoice - 1] . "\n";
-    echo "Race: " . $validRaces[$raceChoice - 1] . "\n";
-
-    $playerInfo['class'] = $validClasses[$classChoice - 1];
-    $playerInfo['race'] = $validRaces[$raceChoice - 1];
-
+        $confirm = strtolower(readline("Is this what you want? (yes/no): "));
+    } while ($confirm !== 'yes');
 
     $fileName = 'player.json';
     file_put_contents($fileName, json_encode($playerInfo, JSON_PRETTY_PRINT));
 
-    echo "Character data has been saved!.\n";
+    echo "Character data has been saved!\n";
 }
 
 function enter_world() {
--- main.php
+++ main.php
@@ -2,7 +2,31 @@
 
 include 'functions.php';
 
-game_start();
-new_game();
-enter_world();
+$validChoice = true;
+
+do {
+    echo "Please make a choice\n
+            1.) Start a new game\n
+            2.) Load Game\n
+            3.) Quit\n";
+
+    $choice = readline(">> ");
+
+    switch ($choice) {
+        case "1":
+            echo "Your game is beginning\n";
+            echo new_game();
+            break;
+        case "2":
+            echo "Your game is now loading\n";
+            break;
+        case "3":
+            echo "The game will now exit\n";
+            exit();
+        default:
+            $validChoice = false;
+            echo "Bad choice, brah. Please choose 1, 2, or 3\n";
+    }
+} while (!$validChoice);
+
 
