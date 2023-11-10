63c807ec84111c470132da7b20ebecdec8bbffca
Chad Peppers <cpeppers@usaid.gov>
2023-11-10 10:57:54 -0600

    Updates

--- test.php
+++ test.php
@@ -1,27 +1,7 @@
 <?php
-function save_player($player) {
-  $jsonString = json_encode($player) . "\n";
-  file_put_contents('player.json', $jsonString);
-}
 
-function load_player($filename) {
-  echo "Loading the file...\n";
-  $json = file_get_contents('player.json');
-  echo $json;
-  echo "Turning json back into an array...\n";
-  $player = json_decode($json, true);
+include "functions.php";
 
-  return $player;
-}
+$playerInfo = json_decode(file_get_contents('player.json'), TRUE);
 
-// Sample PHP array
-$data = array(
-  "name" => "Bob Doe",
-  "age" => 30,
-  "email" => "johndoe@example.com"
-);
-save_player($data);
-
-$player = load_player('player.json');
-var_dump($player);
-?>
+weapon_store($playerInfo);
